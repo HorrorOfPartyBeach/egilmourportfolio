8804421d2cee7260bc42e59f5a10bc85b51b1c49
Emma Gilmour <e.gilmour33@gmail.com>
2019-06-01 14:20:37 +0100

    Swap bootstrap vue for bootstrap 4

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -9,24 +9,84 @@
             </div>
             </div>
         </div>
-        <b-container id="about-me">
+        <div class="container-fluid" id="about-me">
             <h2 id="about-me-h2">Craft to Code Via Carpentry</h2>
             <br>
-            <p>
-                Emma Gilmour is a full stack developer working mainly with JavaScript and frameworks such as React and Vue.
+            <p class="about-me-p">
+                Emma Gilmour is a full stack developer working mainly with JavaScript and JavaScript
+                frameworks. After completing Northcoders; an intensive full-time, full stack coding 
+                bootcamp; she was hired as a full stack developer in January 2019 and has since gained 
+                experience with Vue.js and responsive web design.
+            </p>
+            <p class="about-me-p">
+                Emma's first experience with coding came years before bootcamps and tech startups, in the
+                form of editing the HTML in a MySpace page and a not unimpressive AOL Hometowns Scooby Doo
+                fansite. The opportunity to learn more didn't occur until 2017 when she was accepted onto
+                the first cohort of the TechReturners programme in MediaCity in Manchester. This introductory
+                12 week (one day a week) course covered HTML, CSS, JavaScript, jQuery, basic git and BBC:MicroBit.
+                Through TechReturners, Emma found out about Northcoders and was accepted onto the bootcamp in July
+                2018.
+            </p>
+            <p class="about-me-p">
+                Northcoders is a JavaScript based bootcamp, covering basic JavaScript, back end tech
+                (postgreSQL, MongoDB, Express, Node.js), front end tech (React, Bootstrap) and testing suites/libraries
+                (Mocha, Chai, Jest, Cypress)
 
             </p>
-        </b-container>
+        </div>
+
+        <!-- Projects -->
+
+        <div class="container-fluid" id="projects">
+            <div class="row">
+                <div class="col-xs-4">
+                    <div class="project-divs">
+                    <p class="project-divs-p">
+                        Project 1
+                    <br>
+                        This Website
+                    </p>
+                    </div>
+                </div>
+                <div class="col-xs-4">
+                    <div class="project-divs">
+                    <p class="project-divs-p">
+                        Project 2
+                    <br>
+                        Daisy
+                    </p>
+                    </div>
+                </div>
+                <div class="col-xs-4">
+                    <div class="project-divs">
+                    <p class="project-divs-p">
+                        Project 3
+                    <br>
+                        Non-coding projects
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <FooterComp />
     </div>
 </template>
 
 <script>
+import FooterComp from './FooterComp.vue';
 
+export default {
+    components: {
+        'footerComp': FooterComp
+    },
+}
 </script>
 
 
 <style scoped>
+html {
+  scroll-behavior: smooth;
+}
 /********* Simple overlay START*******/
 /* Main container */
 .overlay-image {
@@ -74,10 +134,43 @@
 
 /********* About Me Section START *******/
 #about-me {
-    padding-top: 10px;
-    padding-bottom: 10px;
+    padding-top: 20px;
+    padding-bottom: 20px;
     background: #000;
     color: whitesmoke;
+    text-align: center;
+}
+.about-me-p {
+    padding-left: 100px;
+    padding-right: 100px;
 }
 /********* About Me Section END *******/
+
+/********* Projects Section START *******/
+#projects {
+    padding-top: 20px;
+    padding-bottom: 20px;
+    background: orangered;
+    color: whitesmoke;
+    text-align: center;
+    display: flex;
+}
+.project-cols .col {
+    text-align: center;
+}
+/* .col-md-4 {
+} */
+.project-divs {
+    width: 120px;
+    height: 120px;
+    border-radius: 100px;
+    background: #000;
+    display: inline-block;
+}
+.project-divs-p {
+    text-align: center;
+    vertical-align:text-bottom;
+    padding-top: 20px;
+}
+/********* Projects Section END *******/
 </style>
