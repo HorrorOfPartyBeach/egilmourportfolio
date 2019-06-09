f50251112733166469f44424c0cc6a72f6c27ed4
Emma Gilmour <e.gilmour33@gmail.com>
2019-06-09 18:31:40 +0100

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const webpack = require('webpack');
-var path = require('path');
 
 module.exports = {
-  entry: [
-    path.resolve(__dirname, "src/main.js"),
-  ],
+  entry: './src/main.js',
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
