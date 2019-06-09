89e57d8ea5387f32ceb2d61a72cdf19158e676d2
Emma Gilmour <e.gilmour33@gmail.com>
2019-06-09 17:57:38 +0100

    Alter webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: [
+    path.resolve(__dirname, "src/main.js"),
+  ],
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader' },
