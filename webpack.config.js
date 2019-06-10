7cf17100ed0412c4a0c1a446e8e64e3418c4b3ac
Emma Gilmour <e.gilmour33@gmail.com>
2019-06-10 18:34:33 +0100

    add bootstrap npm

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,30 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, use: { loader: 'babel-loader'} },
       { test: /\.vue$/, use: 'vue-loader' },
       { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+      {
+        test: /\.png$/,
+        use: "url-loader?limit=100000"
+      },
+      {
+         test: /\.jpg$/,
+         use: "file-loader"
+      },
+     {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+         use: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+         use: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }
     ]
   },
   resolve: {
@@ -20,9 +44,6 @@ module.exports = {
   devServer: {
     open: true,
     hot: true,
-  },
-  externals: {
-    bootstrap: 'bootstrap'
   },
   plugins: [
     new HtmlWebpackPlugin({
